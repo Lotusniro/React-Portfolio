de5ff0b89404e42f3ee617e6931a7f3cfcffd186
Lotus <niroshifinance@gmail.com>
2024-03-03 00:11:12 +0000

    images folder changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/pages/Header/Header';
 import Home from './components/pages/Home/Home';
 import Contact from './components/pages/Contact/Contact';
+
 import ProjectGallery from './components/pages/ProjectGallery/ProjectGallery';
 import Footer from './components/pages/Footer/Footer';
 
